a097e591749e32d937ba1d8f05e0385bfd4f0d3b
bugg86 <jackfink68@yahoo.com>
2026-02-10 17:05:30 -0500

    add under construction pages to incomplete pages

--- src/app/staff/page.tsx
+++ src/app/staff/page.tsx
@@ -2,8 +2,24 @@ import { DesktopNavbar } from "@/components/common/NavBar";
 
 export default function StaffPage() {
   return (
-      <div>
-        <DesktopNavbar />
+    <div
+      className="relative min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url(/bg1.png)" }}
+    >
+      <DesktopNavbar />
+      <div className="pointer-events-none absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center">
+        <img
+          src="/sleep.gif"
+          alt="Sleeping animation"
+          className="h-auto w-[320px]"
+        />
+        <p
+          className="mt-4 text-3xl text-[#FFFCEA] drop-shadow"
+          style={{ fontFamily: "var(--font-sunlight-dreams)" }}
+        >
+          Page under construction!!
+        </p>
       </div>
-    );
+    </div>
+  );
 }
